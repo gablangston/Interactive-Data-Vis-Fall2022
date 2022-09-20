ab39da3e9523d0396c8a4f2b7cd0e1016ed741b4
Gabrielle Langston <the13seasons@gmail.com>
2022-09-20 16:00:25 -0400

    First commit with javascript!

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+// This code controls how to interact with the button
+
+// Loop that increases the counts of clicks
+
+let counter_display = 0
+
+const clickCounts = () => {
+	counter_display = counter_display + 1
+	document.getElementById("counter_display").innerHTML = counter_display;
+}
