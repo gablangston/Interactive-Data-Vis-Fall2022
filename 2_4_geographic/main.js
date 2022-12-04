7e80a3bf9268d6c5cae48d9d60ecda7bf31b9e2f
Gabrielle Langston <the13seasons@gmail.com>
2022-12-04 00:47:31 -0500

    First Map

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.9,
+ height = window.innerHeight * 0.7,
+ margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 /**
  * LOAD DATA
@@ -9,18 +9,49 @@
  * */
  Promise.all([
   d3.json("../data/usState.json"),
-  d3.csv("../data/stateCapitals.csv", d3.autoType),
-]).then(([geojson, capitals]) => {
+  d3.csv("../data/usHeatExtremes.csv", d3.autoType),
+]).then(([geojson, heat]) => {
+  console.log('heat', heat)
+
+   // CREATE SVGS
+  const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+  svg.append("text")
+  .attr("x", (width / 2))             
+  .attr("y", margin.top)
+  .attr("text-anchor", "middle")  
+  .style("font-size", "18px")  
+  .text("Heat Extremes in USA");
   
   // SPECIFY PROJECTION
- 
+  const projection = d3.geoAlbersUsa()
+    .fitSize([width, height], geojson)
 
-  // DEFINE PATH FUNCTION
+   // DEFINE PATH FUNCTION
+  const pathGen = d3.geoPath(projection)
 
-
-  // APPEND GEOJSON PATH  
-  
-  
+    // APPEND GEOJSON PATH  
+  const states = svg.selectAll("path.states")
+    .data(geojson.features)
+    .join("path")
+    .attr("class", "states")
+    .attr("d", coords =>pathGen(coords))
+    .attr("fill", "lightblue")
+    .attr("stroke", "black")
+    
   // APPEND DATA AS SHAPE
+  const heats = svg.selectAll("circle.heats")
+  .data(heat)
+  .join("circle")
+  .attr("class", "heats")
+  .attr("r", 2)
+  .attr("transform", (d) => {
+    const [x,y] = projection([d.Long,d.Lat])
+    return `translate(${x}, ${y})`
+
+})
 
 });
